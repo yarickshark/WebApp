2fe01cc4e58c325463b495dd97f361a67d096fae
yarickshark <asteroid152008@gmail.com>
2023-09-20 22:39:41 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import * as ReactDOMClient from 'react-dom/client';
+import Header from './components/Header';
+
+class App extends React.Component {
+  helpText = "Help text"
+  render() {
+    return (<div className="name">
+    <Header title="Шапка сайта" />
+  <h1>{this.helpText}</h1>
+  <input placeholder={this.helpText} />
+</div>)
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import ReactDOMClient from 'react-dom/client';
-//import './index.css';
-//import App from './App';
-//import reportWebVitals from './reportWebVitals';
-
-const helpText = "Help text"
-
-const elements = (<div className="name">
-  <h1>{helpText}</h1>
-  <input placeholder={helpText} />
-</div>)
-
-//const app = document.getElementById("app")
+import * as ReactDOMClient from 'react-dom/client';
+import App from './App'
+import './css/main.css'
 
 const app = ReactDOMClient.createRoot(document.getElementById("app"))
 
-app.render(elements)
+app.render(<App />)
