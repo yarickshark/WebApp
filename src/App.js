9913f5f3e73ef5087a4a0d485737e6aa8c7464d9
yarickshark <asteroid152008@gmail.com>
2023-09-23 14:15:25 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ const DateTimePicker = () => {
   return (
     <div>
 
-      tg.initData <br /><br />
+      {tg.initData} <br /><br />
 
       <label htmlFor="date">Оберіть дату:</label>
       <select id="date" onChange={handleDateChange}>
