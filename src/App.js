f8a144c14b37562b0e5d2bab0e5cc00bf764fc57
yarickshark <asteroid152008@gmail.com>
2023-09-28 23:25:13 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -98,6 +98,7 @@ const DateTimePicker = () => {
     <div>
 
       Вітаю, {tg.initDataUnsafe.user.username} <br /><br />
+      {tg.initDataUnsafe.user.first_name} <br /><br />
 
       <label htmlFor="date">Оберіть дату:</label>
       <select id="date" onChange={handleDateChange}>
